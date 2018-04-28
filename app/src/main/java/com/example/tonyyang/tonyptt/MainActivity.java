ed5b8d619ae54cdb28850404f373438c5577e520
Tony Yang <superzoro168@gmail.com>
2018-04-28 17:06:36 +0800

    Initialize drawer and toolbar

--- app/src/main/java/com/example/tonyyang/tonyptt/MainActivity.java
+++ app/src/main/java/com/example/tonyyang/tonyptt/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.tonyyang.tonyptt;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DrawerLayout mDrawerLayout;
+
+    private Toolbar mToolbar;
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureToolbar();
+        configureDrawer();
+    }
+
+    private void configureToolbar() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void configureDrawer() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationDrawerFragment = new NavigationDrawerFragment();
+        getFragmentManager().beginTransaction().replace(R.id.navigation_drawer, mNavigationDrawerFragment).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
