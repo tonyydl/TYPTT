365d0fdbc29fa88a79b7ea958dd6a4baa13c01b6
Tony Yang <superzoro168@gmail.com>
2019-11-24 15:34:05 +0800

    Fix toolbar and add layout change tranition

--- app/src/main/java/com/tonyyang/typtt/KotlinExtensions.kt
+++ app/src/main/java/com/tonyyang/typtt/KotlinExtensions.kt
@@ -1,6 +1,5 @@
 package com.tonyyang.typtt
 
-import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.disposables.CompositeDisposable
@@ -10,8 +9,7 @@ fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
     compositeDisposable.add(this)
 }
 
-fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.() -> Unit) {
-    setSupportActionBar(findViewById(toolbarId))
+fun AppCompatActivity.expandActionBar(action: ActionBar.() -> Unit) {
     supportActionBar?.run {
         action()
     }
--- app/src/main/java/com/tonyyang/typtt/ui/MainActivity.kt
+++ app/src/main/java/com/tonyyang/typtt/ui/MainActivity.kt
@@ -2,16 +2,15 @@ package com.tonyyang.typtt.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.tonyyang.typtt.R
-import com.tonyyang.typtt.setupActionBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupActionBar(R.id.toolbar) {
-            setDisplayHomeAsUpEnabled(false)
-        }
+        setSupportActionBar(toolbar as Toolbar)
     }
 }
